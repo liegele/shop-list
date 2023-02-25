05c2a8881adaefdde136b255804fc58472941a1e
liegele <liegele@gmail.com>
2023-02-25 18:47:12 -0300

    Atualizando layout após instalação do app no smartphone.

--- script.js
+++ script.js
@@ -162,11 +162,11 @@ items.forEach((item) => {
 
 //Registering serviveWorker.js
 
-/* if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker
       .register('/serviceWorker.js')
       .then((res) => console.log('service worker registered'))
       .catch((err) => console.log('service worker not registered', err));
   });
-} */
+}
