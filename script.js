98d24ffe7d38b2bc0aa5ff7ddbf4f752e4573e35
liegele <liegele@gmail.com>
2023-02-25 21:56:45 -0300

    Atualizando swipe com HammerJS.

--- script.js
+++ script.js
@@ -200,11 +200,11 @@ items.forEach((item) => {
 
 //Registering serviveWorker.js.
 
-/* if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker
       .register('/serviceWorker.js')
       .then((res) => console.log('service worker registered'))
       .catch((err) => console.log('service worker not registered', err));
   });
-} */
+}
