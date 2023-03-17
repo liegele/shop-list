ea849c27b2ad4c959117f62e93cf7faafb078e04
liegele <liegele@gmail.com>
2023-03-16 22:51:53 -0300

    Implementando filtro por categoria e mudanças no layout dos botões do menu inferior

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ const categorySelect = document.getElementById('category-select');
 const itemInput = document.getElementById('item-input');
 const snackbar = document.getElementById('snackbar');
 let itemsHtml = document.getElementById('items');
+var itemsHtmlCopy = document.getElementById('items');
 
 //Wakelock checking
 
@@ -58,8 +59,6 @@ wakeLockScreen();
 document.addEventListener('visibilitychange', async () => {
   if (wakeLock !== null && document.visibilityState === 'visible') {
     wakeLock = await navigator.wakeLock.request('screen');
-    // console.log(' ---> ', wakeLock);
-    // console.log('wake lock active...');
   }
 });
 
@@ -93,149 +92,85 @@ let listItem = {
   selected: 'false',
 };
 
-//Getting items from collection in Firestore
-let items;
-const getItems = function () {
-  db.collection(userId)
-    .orderBy('category', 'desc')
-    .onSnapshot((snapshot) => {
-      snapshot.docChanges().forEach((change) => {
-        const data = { id: change.doc.id, ...change.doc.data() };
-        console.log('===> ', data);
+//Filtering list item based on category seleted.
 
-        if (change.type === 'added') {
-          /* const data = snapshot.docs.map((item) => ({
-            id: item.id,
-            ...item.data(),
-          })); */
-
-          // itemsHtml.innerHTML = '';
-          // for (let i = 0; i < data.length; i++) {
-          let html = `
-            <div id="${data.id}" class="list-item" data-category="${
-            data.category
-          }">
-              <button id="S${data.id}" name="settings-button" class="settings">
-                <div class="list-icon">
-                  <i class="bx bx-list-check bx-md"></i>
-                </div>
-              </button>
-              <div class="list-content">
-                <div name="left-icon" class="bx ${
-                  data.selected === 'true'
-                    ? 'bxs-checkbox-checked'
-                    : 'bx-checkbox'
-                } bx-lg list-category-0"></div>
-                <div class="caption">
-                  <h5 class="truncate">${data.name}</h5>
-                  <p>${category[data.category]}</p>
-                </div>
-                <div name="right-icon" class="amount">
-                  <i class="bx bx-plus-circle bx-sm"></i>
-                  <i class="bx bx-minus-circle bx-sm"></i>
-                </div>
-              </div>
-              <button id="D${data.id}" name="delete-button" class="delete">
-                <div class="list-icon">
-                  <i class="bx bxs-trash"></i>
-                </div>
-              </button>
-            </div>`;
-          // itemsHtml.innerHTML += html;
-          itemsHtml.insertAdjacentHTML('afterbegin', html);
-          // console.log(data.name);
-
-          settingListItemEventListener(data.id, data.selected);
-
-          // //Ading event listener to DELETE button.
-          // document
-          //   .getElementById(`D${data.id}`)
-          //   .addEventListener('click', (e) => {
-          //     db.collection(userId).doc(data.id).delete();
-          //     console.log('delete item');
-          //   });
-
-          // //Selecting item from list in order to able to put it in SHOPLIST
-          // document
-          //   .getElementById(`S${data.id}`)
-          //   .addEventListener('click', (e) => {
-          //     db.collection(userId)
-          //       .doc(data.id)
-          //       .update({
-          //         selected: data.selected === 'true' ? 'false' : 'true',
-          //       });
-          //     console.log(
-          //       'select item: ',
-          //       data.selected === 'true' ? 'false' : 'true'
-          //     );
-          //   });
-
-          // // Add Swipe to items
-          // settingSwipe(data.id);
-          // // }
-        }
-
-        if (change.type === 'modified') {
-          //maybe later select by MODE (select, select)...
-          const itemModified = document.getElementById(data.id);
-          console.log(data);
-          itemModified.innerHTML = `
-          <button id="S${data.id}" name="settings-button" class="settings">
+//Item list Component function
+const itemListComponent = function (
+  idData,
+  selectedData,
+  categoryData,
+  nameData
+) {
+  return `<button id="S${idData}" name="settings-button" class="settings">
             <div class="list-icon">
               <i class="bx bx-list-check bx-md"></i>
             </div>
           </button>
           <div class="list-content">
             <div name="left-icon" class="bx ${
-              data.selected === 'true' ? 'bxs-checkbox-checked' : 'bx-checkbox'
+              selectedData === 'true' ? 'bxs-checkbox-checked' : 'bx-checkbox'
             } bx-lg list-category-0"></div>
             <div class="caption">
-              <h5 class="truncate">${data.name}</h5>
-              <p>${category[data.category]}</p>
+              <h5 class="truncate">${nameData}</h5>
+              <p>${category[categoryData]}</p>
             </div>
             <div name="right-icon" class="amount">
               <i class="bx bx-plus-circle bx-sm"></i>
               <i class="bx bx-minus-circle bx-sm"></i>
             </div>
           </div>
-          <button id="D${data.id}" name="delete-button" class="delete">
+          <button id="D${idData}" name="delete-button" class="delete">
             <div class="list-icon">
               <i class="bx bxs-trash"></i>
             </div>
           </button>`;
+};
+
+//Getting items from collection in Firestore
+let items;
+const getItems = function (categoryID, condition) {
+  itemsHtml.innerHTML = '';
+  db.collection(userId)
+    .where('category', `${condition}`, `${categoryID}`)
+    // .orderBy('category', 'desc')
+    .onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        const data = { id: change.doc.id, ...change.doc.data() };
+        // console.log('===> ', data);
+
+        if (change.type === 'added') {
+          let html = `
+            <div id="${data.id}" class="list-item" data-category="${
+            data.category
+          }">
+              ${itemListComponent(
+                data.id,
+                data.selected,
+                data.category,
+                data.name
+              )}
+            </div>`;
+          // itemsHtml.innerHTML += html;
+          itemsHtml.insertAdjacentHTML('afterbegin', html);
+          // console.log(data.name);
 
           settingListItemEventListener(data.id, data.selected);
-
-          // //Ading event listener to DELETE button.
-          // document
-          //   .getElementById(`D${data.id}`)
-          //   .addEventListener('click', (e) => {
-          //     db.collection(userId).doc(data.id).delete();
-          //     console.log('delete item');
-          //   });
-
-          // //Selecting item from list in order to able to put it in SHOPLIST
-          // document
-          //   .getElementById(`S${data.id}`)
-          //   .addEventListener('click', (e) => {
-          //     /* document.querySelector('.list-content')[0].style.animationName =
-          //       'blue-animation';
-          //     console.log('----->', document.getElementById(data.id)); */
-          //     db.collection(userId)
-          //       .doc(data.id)
-          //       .update({
-          //         selected: data.selected === 'true' ? 'false' : 'true',
-          //       });
-          //     console.log(
-          //       'select item: ',
-          //       data.selected === 'true' ? 'false' : 'true'
-          //     );
-          //     // blueAnimation.play();
-          //   });
-
-          // // Add Swipe to items
-          // settingSwipe(data.id);
         }
+
+        if (change.type === 'modified') {
+          //maybe later select by MODE (select, select)...
+          const itemModified = document.getElementById(data.id);
+          console.log(data);
+          itemModified.innerHTML = itemListComponent(
+            data.id,
+            data.selected,
+            data.category,
+            data.name
+          );
+
+          settingListItemEventListener(data.id, data.selected);
+        }
+
         if (change.type === 'removed') {
           //Removing childNode from parentNode (items)
           const parentItemRemoved = document.getElementById('items');
@@ -258,10 +193,39 @@ const getItems = function () {
           makeShop();
           break;
       }
+
+      // itemsHtmlCopy = itemsHtml.cloneNode(true);
+      // console.log(document.querySelector('#items').childElementCount);
+      // console.log('-----> ', itemsHtmlCopy.children.item(19).dataset.category);
+      // itemsHtml.innerHTML = '';
+      // filteredListItems('4');
+      // itemsToBeFiltered = document.querySelectorAll('.list-item');
+      // console.log('>>>>>> ', itemsToBeFiltered);
       // currentMode === 'select' ? selectItems() : null;
+      // itemsToBeFiltered = document.getElementsByClassName('list-item');
     });
 };
 
+// console.log('OUT-----> ', itemsHtmlCopy.childElementCount);
+// console.log('-----> ', itemsHtmlCopy.children.item(19).dataset.category);
+
+//Defining function to filter list items according to category select.
+/* const filteredListItems = function (categoryID) {
+  document.getElementById('items').innerHTML = '';
+  const filters = document.querySelectorAll('.scrollmenu > a');
+  console.log(filters);
+  filters.forEach((item) => {
+    console.log(item);
+    item.addEventListener('click', () => {
+      getItems(item.id);
+    });
+  });
+  // getItems(categoryID);
+};
+
+filteredListItems('0'); */
+
+//Defining function to add eventlistener to list items.
 const settingListItemEventListener = function (idData, selectedData) {
   //Ading event listener to DELETE button.
   document.getElementById(`D${idData}`).addEventListener('click', (e) => {
@@ -284,78 +248,7 @@ const settingListItemEventListener = function (idData, selectedData) {
   // }
 };
 
-/* const getItems = function () {
-  db.collection(userId)
-    .orderBy('created', 'desc')
-    .onSnapshot((snapshot) => {
-      const data = snapshot.docs.map((item) => ({
-        id: item.id,
-        ...item.data(),
-      }));
-      console.log(data);
-      itemsHtml.innerHTML = '';
-      for (let i = 0; i < data.length; i++) {
-        let html = `
-        <div class="list-item" data-category="${data[i].category}">
-          <button id="S${data[i].id}" name="settings-button" class="settings">
-            <div class="list-icon">
-              <i class="bx bx-list-check bx-md"></i>
-            </div>
-          </button>
-          <div class="list-content">
-            <div name="left-icon" class="bx ${
-              data[i].selected === 'true'
-                ? 'bxs-checkbox-checked'
-                : 'bx-checkbox'
-            } bx-lg list-category-0"></div>
-            <div class="caption">
-              <h5 class="truncate">${data[i].name}</h5>
-              <p>Quantidade: ${data[i].amount}</p>
-            </div>
-            <div name="right-icon" class="amount">
-              <i class="bx bx-plus-circle bx-sm"></i>
-              <i class="bx bx-minus-circle bx-sm"></i>
-            </div>
-          </div>
-          <button id="D${data[i].id}" name="delete-button" class="delete">
-            <div class="list-icon">
-              <i class="bx bxs-trash"></i>
-            </div>
-          </button>
-        </div>`;
-        itemsHtml.insertAdjacentHTML('beforeend', html);
-        console.log(data[i].name);
-
-        //Delete item list and Add event listener to DELETE button.
-        document
-          .getElementById(`D${data[i].id}`)
-          .addEventListener('click', (e) => {
-            db.collection(userId).doc(data[i].id).delete();
-            console.log('delete item');
-          });
-
-        //Selecting item from list in order to able to put it in SHOPLIST
-        document
-          .getElementById(`S${data[i].id}`)
-          .addEventListener('click', (e) => {
-            db.collection(userId)
-              .doc(data[i].id)
-              .update({
-                selected: data[i].selected === 'true' ? 'false' : 'true',
-              });
-            console.log(
-              'select item: ',
-              data[i].selected === 'true' ? 'false' : 'true'
-            );
-          });
-      }
-
-      //To querySelectorAll work and get all list-items in a NodeList()...
-      settingSwipe();
-
-      currentMode === 'select' ? selectItems() : null;
-    });
-}; */
+// filteredListItems('0');
 
 //------------------------------------------------------------
 //ADD ITEMS MODE
@@ -395,15 +288,6 @@ clearButton.addEventListener('click', () => {
   categorySelect.value = itemInput.value = '';
 });
 
-/* let blueAnimation = anime
-  .timeline({
-    endDelay: 1000,
-    easing: 'easeInOutQuad',
-    direction: 'alternate',
-    loop: true,
-  })
-  .add({ targets: '.list-content', background: '#3096d1' }, 0); */
-
 let slidedown = anime({
   targets: '.list-add-item',
   translateY: ['-150px', '0px'],
@@ -427,7 +311,6 @@ let slideup = anime({
 //Function to add item data to collection using SET()
 const addListItem = function () {
   db.collection(userId).add(listItem);
-  // getItems();
 };
 
 //Add item data to collection after click on save
@@ -452,7 +335,7 @@ const selectItems = function () {
   toggleElements('delete-button', 'delete-invisible', 'delete');
   toggleElements('left-icon', 'list-category-0', 'list-category-0-invisible');
   toggleElements('left-icon', 'bx-checkbox', 'bx-cart');
-  toggleElements('right-icon', 'amount', 'amount-invisible');
+  toggleElements('right-icon', 'amount-invisible', 'amount');
   toggleElements('add-items', 'navbar-item', 'navbar-item-blue');
   toggleElements('select-items', 'navbar-item-blue', 'navbar-item');
   toggleElements('make-shop', 'navbar-item', 'navbar-item-blue');
@@ -486,8 +369,35 @@ makeShopButton.addEventListener('click', makeShop);
 //------------------------------------------------------------
 
 //Setting SelectItems mode as main view at starting up.
-window.document.addEventListener('DOMContentLoaded', getItems);
-// window.addEventListener('load', selectItems);
+window.document.addEventListener('DOMContentLoaded', getItems(0, '>='));
+
+//Defining function to filter list items according to category select.
+const filteredListItems = function () {
+  // document.getElementById('items').innerHTML = '';
+  const filters = document.querySelectorAll('.scrollmenu > a');
+  console.log(document.querySelector('#items').childElementCount);
+
+  for (const child of itemsHtml.children) {
+    console.log('---> ', i);
+  }
+
+  filters.forEach((item) => {
+    // console.log(item);
+    item.addEventListener('click', () => {
+      console.log('Show category: ' + item.id);
+      // const condition = item.id === '-1' ? '>=' : '==';
+      if (item.id === '-1') {
+        getItems(0, '>=');
+      } else {
+        getItems(item.id, '==');
+      }
+      // console.log(condition);
+      // getItems(item.id, condition);
+    });
+  });
+};
+
+filteredListItems();
 
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 let prevScrollpos = window.pageYOffset;
@@ -516,12 +426,12 @@ const settingSwipe = function (item) {
     // console.log(ev.type + ' gesture detected.' + ev.deltaX);
     // console.log(ev, currentMode);
     if (ev.deltaX <= -25) {
-      ev.target.style.transform = 'translateX(-100px)';
-      /* anime({
+      // ev.target.style.transform = 'translateX(-100px)';
+      anime({
         targets: ev.target,
         translateX: -100,
-        duration: 300,
-      }); */
+        duration: 200,
+      });
     }
 
     /* items.forEach((item) => {
@@ -538,61 +448,15 @@ const settingSwipe = function (item) {
     }); */
 
     if (ev.type === 'swiperight') {
-      ev.target.style.transform = 'translateX(0)';
-      /* anime({
+      // ev.target.style.transform = 'translateX(0)';
+      anime({
         targets: ev.target,
         translateX: 0,
-        duration: 300,
-      }); */
+        duration: 200,
+      });
     }
   });
 };
-
-/* const settingSwipe = function () {
-  items = document.querySelectorAll('.list-item');
-
-  //Adding swipe to items of the list
-  items.forEach((item) => {
-    let mc = new Hammer(item);
-    // listen to events...
-    mc.on('swipeleft swiperight tap', function (ev) {
-      // mc.on('swipe panleft panright tap press', function (ev) {
-      // console.log(ev.type + ' gesture detected.' + ev.deltaX);
-      // console.log(ev, currentMode);
-      if (ev.deltaX <= -25) {
-        // ev.target.style.transform = 'translateX(-100px)';
-        anime({
-          targets: ev.target,
-          translateX: -100,
-          duration: 300,
-        });
-      }
-
-      items.forEach((item) => {
-        let content = item.querySelector('.list-content');
-
-        if (content === ev.target) {
-          return null;
-        }
-
-        anime({
-          targets: content,
-          translateX: 0,
-        });
-      });
-
-      if (ev.type === 'tap' || ev.type === 'swiperight') {
-        // ev.target.style.transform = 'translateX(0)';
-        anime({
-          targets: ev.target,
-          translateX: 0,
-          duration: 300,
-        });
-      }
-    });
-  });
-};
- */
 
 //Vibrating for 200ms.
 const vibration = function () {
@@ -623,27 +487,3 @@ const showSnackbar = function (msg, show) {
       .catch((err) => console.log('service worker not registered', err));
   });
 } */
-
-/* let list = {
-  created: firebase.firestore.FieldValue.serverTimestamp(),
-  modified: '',
-  accessPermission: [
-    {
-      email: '',
-      mode: '',
-    },
-  ],
-  listItems: [
-    {
-      category: '',
-      name: '',
-      amount: '',
-      price: '0.00',
-      priceTimestamp: '',
-      lastPrice: '0.00',
-      lastPriceTimestamp: '',
-      selected: 'false',
-      search: '',
-    },
-  ],
-}; */
